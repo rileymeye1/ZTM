010549fe5c655d13bcd3877ffb328ff60a2a00f1
Riley Meyer <134163111+rileymeye1@users.noreply.github.com>
2024-08-27 19:10:24 -0500

    JavaScript Learning
    Functions
    Data Structures
    Arrays
    Objects
    Loops

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,55 @@
-if (4 + 3 === 7) {
-  alert("You're smart!");
+var database = [
+  {
+    username: "riley",
+    password: "supersecret"
+  },
+  {
+    username: "john",
+    password: "thisIsAwesome"
+  },
+  {
+    username: "bob",
+    password: "Im$oCool!"
+  },
+  {
+    username: "sam",
+    password: "WrongPassword"
+  }
+];
+
+var newsfeed = [
+  {
+    username: "bob",
+    timeline: "You rock dude!"
+  },
+  {
+    username: "john",
+    timeline: "JavaScript rules man!"
+  },
+  {
+    username: "sam",
+    timeline: "Don't be lame, learn JavaScript!"
+  }
+];
+
+function isUserValid(username, password) {
+  for (var i=0; i < database.length; i++) {
+    if (database[i].username === username && database[i].password === password) {
+      return true;
+    }
+  }
+  return false;
 }
 
-console.log("Hello!");
+function signIn(username, password) {
+  if (isUserValid(username, password)) {
+    console.log(newsfeed);
+  } else {
+    alert("Sorry, wrong username and password");
+  }
+}
+
+var userNamePrompt = prompt("What\'s your username?");
+var passwordPrompt = prompt("What\'s your password?");
+
+signIn(userNamePrompt, passwordPrompt);
